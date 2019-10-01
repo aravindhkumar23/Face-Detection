0c463d8ef5353e5de75bb1dc9379295fe8c7b4ff
Aravindh Kumar <aravindhkumar23@gmail.com>
2019-10-01 19:58:46 +0530

    Initial commit for Face detection using kit.

---
+++ README.md
@@ -0,0 +1,48 @@
+# flutter_face_detection_app
+
+A new Flutter application to detect faces using ml kit.
+
+## Getting Started
+
+This project is a starting point for a Flutter application.
+
+A few resources to get you started if this is your first Flutter project:
+
+- [Lab: Write your first Flutter app](https://flutter.dev/docs/get-started/codelab)
+- [Cookbook: Useful Flutter samples](https://flutter.dev/docs/cookbook)
+
+For help getting started with Flutter, view our
+[online documentation](https://flutter.dev/docs), which offers tutorials,
+samples, guidance on mobile development, and a full API reference.
+
+
+# Flutter - Face Detection using ML kit
+
+Hi! in this project **google ML kit** is used to detect faces from the image.
+
+# Dependencies
+
+- [ImagePicker](https://pub.dev/packages/image_picker) - to pick image from gallery or from camera,
+- [ML kit](https://pub.dev/packages/firebase_ml_vision) - A Flutter plugin to use the [ML Kit Vision for Firebase API](https://firebase.google.com/docs/ml-kit/).
+- [Floating action button list](https://pub.dartlang.org/packages/unicorndial) - shows floating button list on floating button clicked.
+
+## Implementation [Android]
+
+**Step-1 :** Create new firebase app to create google-services.json for android and iOS projects and place the file as suggested by firebase [refer docs](https://firebase.google.com/docs/flutter/setup).
+**Step-2:** Add dependencies to flutter projects and get the dependencies.
+**Step-3:** Add ```api 'com.google.firebase:firebase-ml-vision-face-model:17.0.2'``` to app level build.gradle file in android -> dependencies.
+**Step-4:** Make sure to add ```apply plugin: 'com.google.gms.google-services'``` in app build.gradle at end of file and ```classpath 'com.google.gms:google-services:3.2.1'``` in project level build.gradle
+**Step-5:** Build and run the project
+
+## Implementation [iOS]
+
+Not yet implemented will update soon.
+
+All your files and folders are presented as a tree in the file explorer. You can switch from one to another by clicking a file in the tree.
+
+## How it works
+- Pick image from gallery/camera using image picker
+- Get Faces from choosen image from firebase vision face detector.
+- The face detector will return a list of Face class which contains the Rect coordinates.
+- The Painter will then use these coordinates to draw a rectangular box around the faces using these coordinates.
+- full source code available at ```lib/main.dart```.
---
+++ lib/main.dart
@@ -0,0 +1,157 @@
+import 'dart:io';
+
+import 'package:firebase_ml_vision/firebase_ml_vision.dart';
+import 'package:flutter/material.dart';
+import 'package:unicorndial/unicorndial.dart';
+import 'package:image_picker/image_picker.dart';
+import 'dart:ui' as ui;
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  File _imageFile;
+  List<Face> _faces;
+  bool isLoading = false;
+  ui.Image _image;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: isLoading
+          ? Center(child: CircularProgressIndicator())
+          : (_imageFile == null)
+              ? Center(child: Text('No image selected'))
+              : Center(
+                  child: FittedBox(
+                    child: SizedBox(
+                      width: _image.width.toDouble(),
+                      height: _image.height.toDouble(),
+                      child: CustomPaint(
+                        painter: FacePainter(_image, _faces),
+                      ),
+                    ),
+                  ),
+                ),
+      floatingActionButton: UnicornDialer(
+        parentButtonBackground: Colors.blue[700],
+        orientation: UnicornOrientation.VERTICAL,
+        parentButton: Icon(Icons.add),
+        childButtons: _getOptionsMenu(),
+      ),
+    );
+  }
+
+  List<UnicornButton> _getOptionsMenu() {
+    List<UnicornButton> children = [];
+    children.add(
+      _generateOption(
+          iconData: Icons.camera_alt,
+          onPressed: () {
+            _getImageAndDetectFaces(isCamera: true);
+          }),
+    );
+    children.add(
+      _generateOption(
+          iconData: Icons.insert_drive_file,
+          onPressed: () {
+            _getImageAndDetectFaces(isCamera: false);
+          }),
+    );
+
+    return children;
+  }
+
+  Widget _generateOption({IconData iconData, Function onPressed}) {
+    return UnicornButton(
+        currentButton: FloatingActionButton(
+      backgroundColor: Colors.blue[500],
+      mini: true,
+      child: Icon(iconData),
+      onPressed: onPressed,
+    ));
+  }
+
+  void _getImageAndDetectFaces({bool isCamera}) async {
+    final imageFile = await ImagePicker.pickImage(
+        source: isCamera ? ImageSource.camera : ImageSource.gallery);
+    setState(() {
+      isLoading = true;
+    });
+    final image = FirebaseVisionImage?.fromFile(imageFile);
+    final faceDetector = FirebaseVision.instance.faceDetector();
+    List<Face> faces = await faceDetector?.processImage(image);
+    if (mounted) {
+      setState(() {
+        _imageFile = imageFile;
+        _faces = faces;
+        _loadImage(imageFile);
+      });
+    }
+  }
+
+  _loadImage(File file) async {
+    final data = await file.readAsBytes();
+    await decodeImageFromList(data).then(
+      (value) => setState(() {
+        _image = value;
+        isLoading = false;
+      }),
+    );
+  }
+}
+
+class FacePainter extends CustomPainter {
+  final ui.Image image;
+  final List<Face> faces;
+  final List<Rect> rects = [];
+
+  FacePainter(this.image, this.faces) {
+    for (var i = 0; i < faces.length; i++) {
+      rects.add(faces[i].boundingBox);
+    }
+  }
+
+  @override
+  void paint(ui.Canvas canvas, ui.Size size) {
+    final Paint paint = Paint()
+      ..style = PaintingStyle.stroke
+      ..strokeWidth = 15.0
+      ..color = Colors.yellow;
+
+    canvas.drawImage(image, Offset.zero, Paint());
+    for (var i = 0; i < faces.length; i++) {
+      canvas.drawRect(rects[i], paint);
+    }
+  }
+
+  @override
+  bool shouldRepaint(FacePainter oldDelegate) {
+    return image != oldDelegate.image || faces != oldDelegate.faces;
+  }
+}
